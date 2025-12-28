0791e8e9111fe6bbbe3f0d8267b1e10e5d25d9e7
shouta0715 <kshouta0715@gmail.com>
2025-12-28 15:02:23 +0900

    feat: better authの設定を追加

---
+++ apps/api/src/lib/auth/index.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { betterAuthOptions } from "./options";
+import { drizzleDatabase } from "@/helpers/drizzle";
+
+export const auth = (
+  env: CloudflareBindings,
+): ReturnType<typeof betterAuth> => {
+  const db = drizzleDatabase({
+    url: env.DB_URL,
+    authToken: env.DB_AUTH_TOKEN,
+  });
+
+  return betterAuth({
+    ...betterAuthOptions,
+    database: drizzleAdapter(db, { provider: "sqlite" }),
+    baseURL: env.BETTER_AUTH_URL,
+    secret: env.BETTER_AUTH_SECRET,
+    trustedOrigins: [env.CLIENT_URL],
+  });
+};
