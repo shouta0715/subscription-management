326af627a038cad05db3799883acfc3ba9e48668
shouta0715 <kshouta0715@gmail.com>
2025-12-28 11:39:00 +0900

    feat: drizzle instanceを作るhelperを追加

--- apps/api/src/helpers/factory.ts
+++ apps/api/src/helpers/factory.ts
@@ -1,14 +1,13 @@
-import { drizzle } from "drizzle-orm/libsql";
 import { createFactory } from "hono/factory";
-import * as schemas from "@/db/schemas";
+import { drizzleDatabase } from "./drizzle";
 import { AppEnv } from "@/types/app-env";
 
 export const factory = createFactory<AppEnv>({
   initApp: (app) => {
     app.use(async (c, next) => {
-      const db = drizzle({
-        connection: { url: c.env.DB_URL, authToken: c.env.DB_AUTH_TOKEN },
-        schema: schemas,
+      const db = drizzleDatabase({
+        url: c.env.DB_URL,
+        authToken: c.env.DB_AUTH_TOKEN,
       });
       c.set("db", db);
       await next();
