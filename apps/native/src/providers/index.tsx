c8778de00c215af869f7b7b5d27143185cf9f7b4
shouta0715 <kshouta0715@gmail.com>
2026-01-01 14:57:00 +0900

    feat: query client providerの追加

--- apps/native/src/providers/index.tsx
+++ apps/native/src/providers/index.tsx
@@ -3,15 +3,18 @@ import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaListener } from "react-native-safe-area-context";
 import { Uniwind } from "uniwind";
+import { TanstackQueryClientProvider } from "./query-provider";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => (
-  <SafeAreaListener
-    onChange={({ insets }) => {
-      Uniwind.updateInsets(insets);
-    }}
-  >
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <HeroUINativeProvider>{children}</HeroUINativeProvider>
-    </GestureHandlerRootView>
-  </SafeAreaListener>
+  <TanstackQueryClientProvider>
+    <SafeAreaListener
+      onChange={({ insets }) => {
+        Uniwind.updateInsets(insets);
+      }}
+    >
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <HeroUINativeProvider>{children}</HeroUINativeProvider>
+      </GestureHandlerRootView>
+    </SafeAreaListener>
+  </TanstackQueryClientProvider>
 );
