f8abee63dc92c6e8c66ac109f36d72b3562c9796
shouta0715 <kshouta0715@gmail.com>
2026-01-01 15:02:55 +0900

    feat: modelにID生成関数を追加

--- packages/model/src/common/id.ts
+++ packages/model/src/common/id.ts
@@ -3,3 +3,5 @@ import * as v from "valibot";
 export const idSchema = v.pipe(v.string(), v.uuid(), v.brand("Id"));
 
 export type Id = v.InferOutput<typeof idSchema>;
+
+export const generateId = (): Id => v.parse(idSchema, crypto.randomUUID());
