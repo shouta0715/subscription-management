6d93014919ffb31383b293d7bfe57d36be722bfe
shouta0715 <kshouta0715@gmail.com>
2026-01-04 17:39:11 +0900

    fix: text componentでfont styleを使用するように修正

--- apps/native/src/components/native/text.tsx
+++ apps/native/src/components/native/text.tsx
@@ -2,14 +2,12 @@
 import { cn } from "heroui-native";
 import { ComponentProps } from "react";
 import { Text as NativeText } from "react-native";
+import { FontVariantProps, fontStyle } from "../font/style";
 
-type Props = ComponentProps<typeof NativeText> & { bold?: boolean };
+type Props = ComponentProps<typeof NativeText> & FontVariantProps;
 
-const Text = ({ className, bold = false, ...props }: Props) => (
-  <NativeText
-    className={cn("font-normal", bold && "font-bold", className)}
-    {...props}
-  />
+const Text = ({ className, font, bold, ...props }: Props) => (
+  <NativeText className={cn(fontStyle({ font, bold, className }))} {...props} />
 );
 
 export { Text };
