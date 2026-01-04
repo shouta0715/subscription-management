7ff8455c2f5847c293244af2b5be2c168762ab1c
shouta0715 <kshouta0715@gmail.com>
2026-01-04 21:27:57 +0900

    feat: onboarding layoutの追加

---
+++ apps/native/src/app/(onboarding)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="choose-signup" />
+    </Stack>
+  );
+}
+
+export default Layout;
---
+++ apps/native/src/app/(onboarding)/choose-signup.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "@/components/native/text";
+
+export default function Page() {
+  return (
+    <View>
+      <Text>Choose Signup</Text>
+    </View>
+  );
+}
