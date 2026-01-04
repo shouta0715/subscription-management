9331f87bf2234856d1badfa2db55200e39ced7f0
shouta0715 <kshouta0715@gmail.com>
2026-01-04 21:25:54 +0900

    feat: authenticated layoutの追加

---
+++ apps/native/src/app/(authenticated)/dashboard.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "@/components/native/text";
+
+export default function Dashboard() {
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  );
+}
