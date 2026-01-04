237b68141557f9dc1be3df277b8966621e345cfc
shouta0715 <kshouta0715@gmail.com>
2026-01-04 21:28:16 +0900

    feat: 認証とオンボーディングのルーティングを追加

--- apps/native/src/app/_layout.tsx
+++ apps/native/src/app/_layout.tsx
@@ -1,12 +1,32 @@
 import "@/tailwind.css";
 
+import { isNullish } from "@package/lib/guard";
 import { Stack } from "expo-router";
+import { isOnboardingCompleted } from "@/features/onboarding/utils/is-completed";
+import { useSession } from "@/lib/auth-client";
 import { Providers } from "@/providers";
 
 export default function RootLayout() {
+  const { data: session } = useSession();
+  const isAuthenticated = !isNullish(session);
+
   return (
     <Providers>
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Protected guard={!isAuthenticated}>
+          <Stack.Screen name="index" />
+        </Stack.Protected>
+
+        {/* authenticated routes */}
+        <Stack.Protected guard={isAuthenticated && isOnboardingCompleted()}>
+          <Stack.Screen name="(authenticated)" />
+        </Stack.Protected>
+
+        {/* onboarding routes */}
+        <Stack.Protected guard={!isOnboardingCompleted()}>
+          <Stack.Screen name="(onboarding)" />
+        </Stack.Protected>
+      </Stack>
     </Providers>
   );
 }
