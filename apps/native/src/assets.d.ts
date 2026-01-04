1029290e24fede036dcce2b3b32115f651ec3b3a
shouta0715 <kshouta0715@gmail.com>
2026-01-04 17:35:56 +0900

    feat: expo fontの設定を追加

---
+++ apps/native/src/assets.d.ts
@@ -0,0 +1,46 @@
+// 画像ファイルの型定義
+declare module "*.png" {
+    const value: number;
+    export default value;
+  }
+  
+  declare module "*.jpg" {
+    const value: number;
+    export default value;
+  }
+  
+  declare module "*.jpeg" {
+    const value: number;
+    export default value;
+  }
+  
+  declare module "*.gif" {
+    const value: number;
+    export default value;
+  }
+  
+  declare module "*.webp" {
+    const value: number;
+    export default value;
+  }
+  
+  declare module "*.ttf" {
+    const value: number;
+    export default value;
+  }
+  
+  declare module "*.otf" {
+    const value: number;
+    export default value;
+  }
+  
+  declare module "*.woff" {
+    const value: number;
+    export default value;
+  }
+  
+  declare module "*.woff2" {
+    const value: number;
+    export default value;
+  }
+  
