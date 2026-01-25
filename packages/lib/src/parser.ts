335a9f05f2b1ba61ad6409b2184c9c005d47b72e
shouta0715 <kshouta0715@gmail.com>
2026-01-25 13:28:18 +0900

    feat: 配列パーサーとタイムスタンプ変換のサポートを追加
    - parseToArraySchema関数を追加して配列データのバリデーションを簡素化
    - timestampSchemaでstring型からDateへの自動変換をサポート
    - 各パーサー関数にJSDocコメントを追加

--- packages/lib/src/parser.ts
+++ packages/lib/src/parser.ts
@@ -1,12 +1,41 @@
 import * as v from "valibot";
 import type { GenericSchema } from "valibot";
 
+/**
+ * 指定したスキーマに従ってデータをバリデーションし、その結果を返します。
+ *
+ * @template T 使用するスキーマの型
+ * @param {T} schema バリデーションに使用する valibot スキーマ
+ * @param {unknown} data バリデーションするデータ
+ * @returns {v.InferOutput<T>} バリデーションされたデータ
+ */
 export const parseSchema = <T extends GenericSchema>(
   schema: T,
   data: unknown,
 ): v.InferOutput<T> => v.parse(schema, data);
 
+/**
+ * 指定したスキーマに従ってデータを安全にバリデーションし、成功・失敗を含む結果を返します。
+ *
+ * @template T 使用するスキーマの型
+ * @param {T} schema バリデーションに使用する valibot スキーマ
+ * @param {unknown} data バリデーションするデータ
+ * @returns {v.SafeParseResult<T>} バリデーションの安全な結果（成功またはエラーを含む）
+ */
 export const safeParseSchema = <T extends GenericSchema>(
   schema: T,
   data: unknown,
 ): v.SafeParseResult<T> => v.safeParse(schema, data);
+
+/**
+ * 指定したスキーマで配列データをバリデーションし、バリデーション結果の配列を返します。
+ *
+ * @template T 使用するスキーマの型
+ * @param {T} schema 配列の要素に使用する valibot スキーマ
+ * @param {unknown} data バリデーションするデータ（配列）
+ * @returns {v.InferOutput<T>[]} バリデーション済みのデータ配列
+ */
+export const parseToArraySchema = <T extends GenericSchema>(
+  schema: T,
+  data: unknown,
+): v.InferOutput<T>[] => v.parse(v.array(schema), data);
--- packages/model/src/common/timestamp.ts
+++ packages/model/src/common/timestamp.ts
@@ -1,5 +1,12 @@
 import * as v from "valibot";
 
-export const timestampSchema = v.date();
+export const timestampSchema = v.union([
+  v.pipe(
+    v.string(),
+    v.transform((v) => new Date(v)),
+    v.date(),
+  ),
+  v.date(),
+]);
 
 export type Timestamp = v.InferOutput<typeof timestampSchema>;
