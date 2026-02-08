016f715df1928c898467c4d5481510ece76dc752
shouta0715 <kshouta0715@gmail.com>
2026-02-08 20:16:16 +0900

    feat: ヘルプページと設定ページを追加

---
+++ apps/native/src/app/(app)/help.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "@/components/native/text";
+
+export default function Help() {
+  return (
+    <View className="pt-safe">
+      <Text>Help</Text>
+    </View>
+  );
+}
---
+++ apps/native/src/app/(app)/setting.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "@/components/native/text";
+
+export default function Setting() {
+  return (
+    <View>
+      <Text>Setting</Text>
+    </View>
+  );
+}
