d544d73eaf4833aa883fb6a698ad34267d0a56dd
shouta0715 <kshouta0715@gmail.com>
2026-02-08 20:17:05 +0900

    feat: モーダルレイアウトとページを追加

---
+++ apps/native/src/app/(app)/(modal)/add-subscription.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "@/components/native/text";
+
+export default function AddSubscription() {
+  return (
+    <View>
+      <Text>サブスクリプションを追加</Text>
+    </View>
+  );
+}
