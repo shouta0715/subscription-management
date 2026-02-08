64f98d51272ea25e09a26c893abde1a4e4d8d7e4
shouta0715 <kshouta0715@gmail.com>
2026-02-08 20:17:15 +0900

    feat: ヘッダーメニューにナビゲーション機能を追加

--- apps/native/src/app/(app)/_layout.tsx
+++ apps/native/src/app/(app)/_layout.tsx
@@ -4,6 +4,33 @@ function Layout() {
   return (
     <Stack>
       <Stack.Screen name="dashboard" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="setting"
+        options={{
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: "transparent" },
+        }}
+      >
+        <Stack.Screen.BackButton displayMode="minimal" />
+        <Stack.Screen.Title>設定</Stack.Screen.Title>
+      </Stack.Screen>
+      <Stack.Screen
+        name="help"
+        options={{
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: "transparent" },
+        }}
+      >
+        <Stack.Screen.BackButton displayMode="minimal" />
+        <Stack.Screen.Title>ヘルプ</Stack.Screen.Title>
+      </Stack.Screen>
+      <Stack.Screen
+        name="(modal)"
+        options={{
+          presentation: "modal",
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
